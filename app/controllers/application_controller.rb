6d4a413e0923ee98589bb5284c1553b9a24123c2
pagood <217xiaoyu@gmail.com>
2016-04-26 00:29:35 -0400

    add mime

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user,:logged_in?,:radar,:recommended_users,:find_conversation,:is_activated?
+  
   protect_from_forgery with: :null_session
   def current_user
   	if session[:user_id]
