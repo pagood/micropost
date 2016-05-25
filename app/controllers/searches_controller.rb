39d0fc5cf75bf67b81d3f3a1b4a42f63804ded78
pagood <217xiaoyu@gmail.com>
2016-05-25 03:11:07 -0400

    mobile search function done

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
 	def create
 		searcher = Search.new
-		@results = searcher.search(params[:search][:key_word])
+		@results = searcher.search(params[:search][:key_word]).paginate(page:params[:page],per_page:20)
 		respond_to do |format|
 			format .js
 			format.html
